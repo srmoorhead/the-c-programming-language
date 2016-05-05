6ef8a73150cfb82c0b9120fb70b2c827e23d441f
srmoorhead <smoo93@gmail.com>
2016-05-05 11:44:37 -0500

    Updated with symbolic constants

--- fToCForLoop.c
+++ fToCForLoop.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+#define LOWER 0   /* lower bound */
+#define UPPER 300  /* upper bound */
+#define STEP 20  /* step size */
+
 int main() {
   printf("Fahrenheit to Celcius Table with For Loop, Reversed\n");
-  for(int fahr = 300; fahr >= 0; fahr -= 20) {
+  for(int fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
     printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
   }
 }
