a5cabdb103f3d8dcd11b4c76dc0050c0637c7182
Sean Richard Moorhead <smoo93@gmail.com>
2016-06-12 16:50:12 -0500

    Original longest line method.

---
+++ longestLine.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define MAXLEN 1000 /* maximum input line size */
+
+int getOneLine(char line[], int maxline);  // getline is a protected function name
+void copy(char to[], char from[]);
+
+int main() 
+{
+  int len;  /* current line length */
+  int max;  /* maximum length seen so far */
+  char line[MAXLEN];  /* current input line */
+  char longest[MAXLEN];  /* longest line saved here */
+
+  max = 0;
+  while ((len = getOneLine(line, MAXLEN)) > 0) {
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    }
+  }
+  if (max > 0) /* there was a line */
+    printf("%s", longest);
+  return 0;
+}
+
+// Returns the length of the line
+// Changes line[] to the next line of input
+// If return == 0, we have reached EOF
+int getOneLine(char s[], int lim) 
+{
+  int c, i;
+
+  for(i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    s[i] = c;
+
+  if(c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+
+  s[i] = '\0';
+  return i;
+}
+
+// Copies the values in from[] to to[]
+// Assumes to[] is big enough
+void copy(char to[], char from[]) 
+{
+  int i;
+
+  i = 0;
+  while((to[i] = from[i]) != '\0')
+    ++i;
+}
